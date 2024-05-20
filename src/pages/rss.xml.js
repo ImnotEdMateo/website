7341776ec4b7ac9716f39982a7fe18c1fcd288b3
Imnot_EdMateo <MineEstasEdMateo@proton.me>
2024-05-20 09:13:15 -0500

    fix: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Mateo | Blog',
-    description: 'Blog de Mateo.',
+    title: 'Blog de Mateo',
+    description: 'Si no te has dado cuenta, Mateo tiene un blog',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./post/*.md')),
     customData: `<language>es-co</language>`,
