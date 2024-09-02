8250cba38e16fcb2c2383730cec4d8c0843ff698
ImnotEdMateo <mateolopez899@outlook.com>
2024-09-02 10:02:10 -0500

    fix: youtube embed -> youtube-lite-embed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://edmateo.site",
-  integrations: [sitemap()]
+  integrations: [
+    sitemap(),
+  ],
 });
