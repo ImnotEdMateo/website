88704f4a3e24ee39894df2c0519e4495cdcacee9
ImnotEdMateo <mateolopez899@outlook.com>
2024-12-22 10:52:05 -0500

    feat: SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 import mdx from "@astrojs/mdx";
 import expressiveCode from 'astro-expressive-code';
 
 export default defineConfig({
   site: "https://edmateo.site",
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [
     sitemap(),
     expressiveCode({
