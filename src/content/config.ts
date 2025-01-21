ba41814c06b51bb9371468b8101c3a37ff49cb12
ImnotEdMateo <mateolopez899@outlook.com>
2025-01-21 00:44:25 -0500

    feat: a lot of new buttons and links for website

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,9 +9,13 @@ const friends = defineCollection({
   loader: file("src/content/webring-lists/friends.json"),
 });
 
+const coolSites = defineCollection({
+  loader: file("src/content/webring-lists/cool-sites.json"),
+});
+
 const usefulInfo = defineCollection({
   loader: file("src/content/webring-lists/useful-info.json"),
 });
 
 
-export const collections = { owns, friends, usefulInfo };
+export const collections = { owns, friends, coolSites, usefulInfo };
