4b328c24a4bfa6062ea6db1717ab397a92284e7f
ImnotEdMateo <mateolopez899@outlook.com>
2025-06-02 12:41:47 -0500

    fix: style between everywhere

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,25 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import path from 'path';
 
+import expressiveCode from 'astro-expressive-code';
+
 export default defineConfig({
   site: 'https://edmateo.site',
   output: 'server',
+
   adapter: node({
     mode: 'standalone',
   }),
+
   vite: {
     resolve: {
       alias: {
         '@': path.resolve('./src')
       }
     }
-  }
+  },
+
+  integrations: [expressiveCode({
+    themes: ['gruvbox-dark-medium']
+  })]
 });
